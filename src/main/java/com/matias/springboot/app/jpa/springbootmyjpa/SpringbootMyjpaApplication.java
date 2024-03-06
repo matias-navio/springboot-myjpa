aa7714dc2d936f980946e086e620b7b9fa3adafc
Matias Navio <naviomatias2@gmail.com>
2024-03-06 10:45:41 -0300

    initial commit

---
+++ src/main/java/com/matias/springboot/app/jpa/springbootmyjpa/SpringbootMyjpaApplication.java
@@ -0,0 +1,13 @@
+package com.matias.springboot.app.jpa.springbootmyjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMyjpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMyjpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matias/springboot/app/jpa/springbootmyjpa/SpringbootMyjpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matias.springboot.app.jpa.springbootmyjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMyjpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
